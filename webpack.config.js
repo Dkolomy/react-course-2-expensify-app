b6188803587205f7fd189580c5b02d9ffd595e34
Kolomyitsev <dmitry.kolomyitsev@maine.gov>
2021-07-25 23:34:14 -0400

    Webpak includes changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { webpack } = require('webpack');
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
